8410e0c91f298bbef31fbcb52a698a7e0834528c
GrafVishna <grafvishna4@gmail.com>
2023-10-24 18:49:08 +0300

    ScrollSmoother off

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useLayoutEffect, useRef, useState} from "react";
+import React, {useRef} from "react";
 import Header from '@src/components/header/Header'
 import Hero from '@src/pages/home/Hero/Hero'
 import GalleryFour from '@src/pages/home/GalleryFour/GalleryFour'
@@ -11,46 +11,45 @@ import Availability from "@src/pages/home/Availability/Availability.jsx";
 import Team from "@src/pages/home/Team/Team.jsx";
 import PhotosScroll from "@src/pages/home/PhotosScroll/PhotosScroll.jsx";
 import Footer from "@src/components/footer/Footer.jsx";
-import {gsap} from "gsap";
-import {ScrollSmoother} from "gsap/ScrollSmoother";
+// import {ScrollSmoother} from "gsap/ScrollSmoother";
 
-gsap.registerPlugin(ScrollSmoother);
+// gsap.registerPlugin(ScrollSmoother);
 
 const Home = () => {
-
-    const homeRef = useRef();
-    const smoother = useRef();
-
-    // const scrollTo = () => {
-    //     smoother.current.scrollTo('.box-c', true, 'center center');
-    // };
-
-    // useLayoutEffect(() => {
-    //     const ctx = gsap.context(() => {
-    //         smoother.current = ScrollSmoother.create({
-    //             smooth: 2,
-    //             effects: true,
-    //         });
-    //     }, homeRef);
-    //     return () => ctx.revert();
-    // }, []);
-
-    return (
-        <div ref={homeRef} className={styles.wrapper}>
-            <Header/>
-            <div className={styles.page}>
-                <Hero/>
-                <GalleryFour data={galleryData}/>
-                <About/>
-                <ProjectsSort/>
-                <Experience/>
-                <Availability/>
-                <Team/>
-                <PhotosScroll/>
-                <Footer/>
-            </div>
-        </div>
-    )
+	 
+	 const homeRef = useRef();
+	 // const smoother = useRef();
+	 
+	 // const scrollTo = () => {
+	 //     smoother.current.scrollTo('.box-c', true, 'center center');
+	 // };
+	 
+	 // useLayoutEffect(() => {
+	 //     const ctx = gsap.context(() => {
+	 //         smoother.current = ScrollSmoother.create({
+	 //             smooth: 2,
+	 //             effects: true,
+	 //         });
+	 //     }, homeRef);
+	 //     return () => ctx.revert();
+	 // }, []);
+	 
+	 return (
+			<div ref={homeRef} className={styles.wrapper}>
+				 <Header/>
+				 <div className={styles.page}>
+						<Hero/>
+						<GalleryFour data={galleryData}/>
+						<About/>
+						<ProjectsSort/>
+						<Experience/>
+						<Availability/>
+						<Team/>
+						<PhotosScroll/>
+						<Footer/>
+				 </div>
+			</div>
+	 )
 }
 
 export default Home
