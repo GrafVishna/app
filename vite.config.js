3857b79c2191a083d4455b2eead4fbbc998a4fcc
GrafVishna <grafvishna4@gmail.com>
2023-10-30 22:08:48 +0200

    Final animation

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,30 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),],
-  resolve: {
-    alias: {
-      '@src': '/src',
-      '@components': '/src/assets/components',
-      '@pages': '/src/assets/pages',
-      '@data': '/src/assets/data',
-      '@functions': '/src/assets/functions',
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+	 build: {
+			rollupOptions: {
+				 external: ['gsap/ScrollSmoother'] // Додайте сюди модулі, які ви хочете включити в external
+			}
+	 },
+	 plugins: [react(),],
+	 resolve: {
+			alias: {
+				 '@src': '/src',
+				 '@components': '/src/assets/components',
+				 '@pages': '/src/assets/pages',
+				 '@data': '/src/assets/data',
+				 '@functions': '/src/assets/functions',
+			},
+	 },
+	 css: {
+			preprocessorOptions: {
+				 scss: {
+						additionalData: `
         @import './src/assets/scss/mixins'; 
         @import './src/assets/scss/global';`,
-      },
-    },
-  },
+				 },
+			},
+	 },
 })
